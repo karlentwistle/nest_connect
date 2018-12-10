189ff2ca3e693cd2bdc492329af917f8e42f2d87
Karl Entwistle <karl@entwistle.com>
2018-12-10 21:43:29 +0000

    Add some tests for NestConnect API Stream

--- lib/nest_connect/api/stream.rb
+++ lib/nest_connect/api/stream.rb
@@ -1,17 +1,25 @@
 module NestConnect
   class API
     class Stream < API
-      def start
+      def initialize(stdout: STDOUT)
+        @stdout = stdout
+      end
+
+      def run
         connection.get do |request|
           request.headers['Accept'] = 'text/event-stream'
           request.headers['Authorization'] = "Bearer #{access_token}"
           request.headers['Cache-Control'] = 'no-cache'
 
           request.options.on_data = Proc.new do |chunk, overall_received_bytes|
-            puts chunk
+            stdout.write chunk
           end
         end
       end
+
+      private
+
+        attr_reader :stdout
     end
   end
 end
---
+++ spec/api/stream_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+RSpec.describe NestConnect::API::Stream do
+  let(:stdout) { StringIO.new }
+  subject(:stream) { NestConnect::API::Stream.new(stdout: stdout) }
+
+  describe '#run' do
+    it 'outputs the response body' do
+      global_config.access_token = 'foo'
+      stub_request_success
+
+      stream.run
+
+      stdout.rewind
+      expect(stdout.gets).to eql("hello")
+    end
+  end
+
+  def stub_request_success
+    stub_request(:get, "https://developer-api.nest.com/").
+      with(
+        headers: {
+          'Accept'=>'text/event-stream',
+          'Authorization'=>'Bearer foo',
+          'Cache-Control'=>'no-cache',
+        }).
+      to_return(
+        status: 200,
+        body: "hello",
+        headers: {}
+      )
+  end
+end
